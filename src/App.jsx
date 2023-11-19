4210d93689d664218b8b32d94ba1861a23233127
ZikoMh <zikoziko971@gmail.com>
2023-11-19 18:20:18 +0000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+const App = () =>(
+  <main className="relative">
+    nav
+    <section className="xl:padding-1 wide:padding-r paddin-b">
+      hero
+    </section>
+    <section className="padding">
+      Popular product
+    </section>
+    <section className="padding">
+      Super quality
+    </section>
+    <section className="padding">
+      services
+    </section>
+    <section className="padding">
+      special offers
+    </section>
+  </main>
+);
+export default App;
